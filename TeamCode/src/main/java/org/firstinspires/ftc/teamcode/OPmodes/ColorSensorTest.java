16c738723b173f6f44889d237d411239380a56d3
Szab <sirmrvsbi@gmail.com>
2018-02-12 12:31:07 +0200

    initial commit of the new libraries

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPmodes/ColorSensorTest.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.OPmodes;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+/**
+ * Created by Vsbi on 2/12/2018.
+ * This is only a test for the color sensors
+ * Copyright (c) 2018 RoboCorp
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+@TeleOp(name = "ColorTest", group = "Sensor")
+public class ColorSensorTest extends LinearOpMode {
+    NormalizedColorSensor colorSensor = null;
+    float[] hsvValues = new float[3];
+    final float values[] = hsvValues;
+
+    @Override
+    public void runOpMode() {
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "Test");
+        waitForStart();
+
+        while (opModeIsActive()) {
+            NormalizedRGBA colors = colorSensor.getNormalizedColors();
+            Color.colorToHSV(colors.toColor(), hsvValues);
+            telemetry.addLine()
+                    .addData("H", "%.3f", hsvValues[0])
+                    .addData("S", "%.3f", hsvValues[1])
+                    .addData("V", "%.3f", hsvValues[2]);
+            telemetry.addLine()
+                    .addData("a", "%.3f", colors.alpha)
+                    .addData("r", "%.3f", colors.red)
+                    .addData("g", "%.3f", colors.green)
+                    .addData("b", "%.3f", colors.blue);
+            float max = Math.max(Math.max(Math.max(colors.red, colors.green), colors.blue), colors.alpha);
+            colors.red /= max;
+            colors.green /= max;
+            colors.blue /= max;
+            int color = colors.toColor();
+
+            telemetry.addLine("normalized color:  ")
+                    .addData("a", "%02x", Color.alpha(color))
+                    .addData("r", "%02x", Color.red(color))
+                    .addData("g", "%02x", Color.green(color))
+                    .addData("b", "%02x", Color.blue(color));
+            telemetry.update();
+        }
+    }
+}
