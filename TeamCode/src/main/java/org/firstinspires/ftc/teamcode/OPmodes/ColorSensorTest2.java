c7590d1c0d3bdb9b73d9531c88fe7415a7092348
Szab <sirmrvsbi@gmail.com>
2018-02-14 09:27:02 +0200

    Autonomous code getting shape

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPmodes/ColorSensorTest2.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.OPmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+/**
+ * Copyright (c) RoboCorp 2018
+ *
+ * Created by Vsbi on 2/14/2018.
+ *
+ *
+ * The only sole purpose of this code is testing therefor IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+  */
+
+public class ColorSensorTest2 extends LinearOpMode{
+    ColorSensor color_sensor;
+    color_sensor = hardwareMap.get()
+}
