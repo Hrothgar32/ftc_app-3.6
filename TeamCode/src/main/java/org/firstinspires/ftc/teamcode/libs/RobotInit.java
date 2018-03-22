5705f657abaf8354b5912cefa92e09122b48a79f
Szab <sirmrvsbi@gmail.com>
2018-03-22 07:51:01 +0200

    final push before Bucharest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/RobotInit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/RobotInit.java
@@ -50,14 +50,14 @@ import static java.lang.Math.min;
         * in the Software without restriction, including without limitation the rights
         * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
         * copies of the Software, and to permit persons to whom the Software is
-        * furnished to do so, subject to the followin   g conditions:
+        * furnished to do so, subject to the following conditions:
         *
         * The above copyright notice and this permission notice shall be included in all
         * copies or substantial portions of the Software.
         *
         * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
         * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
         * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
         * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -274,7 +274,7 @@ public class RobotInit{
             relicTrackables.activate();
         }
         public String identifyVuMark(){
-            while(this.steps < 25) {
+            while(true) {
                 RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
                 if (vuMark == RelicRecoveryVuMark.CENTER) {
                     return "center";
@@ -285,9 +285,9 @@ public class RobotInit{
                 else if(vuMark == RelicRecoveryVuMark.RIGHT){
                     return "right";
                 }
-                this.steps++;
+
             }
-            return "center";
+
         }
     }
 
